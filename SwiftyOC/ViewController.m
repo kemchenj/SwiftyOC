9a4108e4894cb7e7abe6e4de273e801ee2566890
kemchenj <chenkem95@outlook.com>
2017-06-21 20:35:38 +0800

    feat: 添加类型安全，有代码提示的 keypath

--- SwiftyOC/ViewController.m
+++ SwiftyOC/ViewController.m
@@ -18,15 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+        // 变量直接 var 出来，不用再写 emptyViewArray 的类型
     var emptyViewArray = [[NSMutableArray alloc] init];
 
     var subviews = self.view.subviews;
 
+        // foreach 直接循环，不用声明 view 的类型
     foreach(view, subviews) {
         [emptyViewArray addObject:view];
     }
 
     NSLog(@"%@", emptyViewArray);
+
+        // 类型安全，可以自动补全的 keypath
+    [emptyViewArray addObserver:self
+                     forKeyPath:KEYPATH(emptyViewArray, count)
+                        options:NSKeyValueObservingOptionNew
+                        context:nil];
 }
 
 
